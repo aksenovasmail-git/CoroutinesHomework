1636d94312c02217f92784cd3fe7c005ac08bac5
annaksenova <aksenovasmail@gmail.com>
2026-01-08 13:08:10 +0300

    Задание 2.1

--- app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
@@ -3,11 +3,17 @@ package ru.otus.coroutineshomework.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import ru.otus.coroutineshomework.ui.login.data.Credentials
 
 class LoginViewModel : ViewModel() {
 
     private val _state = MutableLiveData<LoginViewState>(LoginViewState.Login())
     val state: LiveData<LoginViewState> = _state
+    private val loginApi = LoginApi()
 
     /**
      * Login to the network
@@ -15,13 +21,33 @@ class LoginViewModel : ViewModel() {
      * @param password user password
      */
     fun login(name: String, password: String) {
-        // TODO: Implement login
+        viewModelScope.launch {
+            _state.value = LoginViewState.LoggingIn
+            try {
+                val user = withContext(Dispatchers.IO) {
+                    loginApi.login(Credentials(name, password))
+                }
+                _state.value = LoginViewState.Content(user)
+            } catch (e: Exception) {
+                _state.value = LoginViewState.Login(error = e)
+            }
+        }
     }
 
     /**
      * Logout from the network
      */
     fun logout() {
-        // TODO: Implement logout
+        viewModelScope.launch {
+            _state.value = LoginViewState.LoggingOut
+            try {
+                withContext(Dispatchers.IO) {
+                    loginApi.logout()
+                }
+                _state.value = LoginViewState.Login()
+            } catch (e: Exception) {
+                _state.value = LoginViewState.Login(error = e)
+            }
+        }
     }
 }
